d470ff122773224eac91f0879474890c67feeef4
1479839090 <54837721+1479839090@users.noreply.github.com>
2020-11-14 13:13:09 -0800

    Update goalstarterMVP.test.js

--- goalstarterMVP.test.js
+++ goalstarterMVP.test.js
@@ -119,7 +119,7 @@ describe('testing wrong login',function(){
 })
 
 describe('testing GET userid',function(){
-  it('responds to POST /home/create_goal/:123',async(done)=>{
+  it('responds to GET /home/create_goal/:123',async(done)=>{
       const response=await request.get('/home/create_goal/:123');
       expect(response.status).toBe(200); 
       done();    
@@ -128,42 +128,42 @@ describe('testing GET userid',function(){
 
 
 describe('testing GET with wrong userid',function(){
-  it('responds to POST /home/create_goal/:321',async(done)=>{
+  it('responds to GET /home/create_goal/:321',async(done)=>{
       const response=await request.get('/home/create_goal/:321');
       expect(response.status).toBe(404); 
       done();    
   })
 })
 
-describe('testing PUT with userid',function(){
-  it('responds to POST /home/comment/:123',async(done)=>{
+describe('testing PUT on comment with right userid',function(){
+  it('responds to PUT /home/comment/:123',async(done)=>{
     const Body= {id: "321", author: "Steven Huang", comment: "abcdesef"}
-      await request.post('/home/comment/:123').send(Body).expect(200)  
+      await request.put('/home/comment/:123').send(Body).expect(200)  
       done(); 
   })
 })
 
 
-describe('testing PUT with wrong userid',function(){
-  it('responds to POST /home/comment/:321',async(done)=>{
+describe('testing PUT ont comment with wrong userid',function(){
+  it('responds to PUT /home/comment/:321',async(done)=>{
     const Body= {id: "3211213131", author: "Not exist", comment: "ccdddd"}
-      await request.post('/home/comment/:321').send(Body).expect(404);
+      await request.put('/home/comment/:321').send(Body).expect(404);
       done();    
   })
 })
 
-describe('testing PUT with wrong userid',function(){
-  it('responds to POST /home/create_goal/:userid',async(done)=>{
+describe('testing PUT on like with right userid',function(){
+  it('responds to PUT /home/create_goal/:userid',async(done)=>{
     const Body= {id: "123"}
-      await request.post('/home/like/:123').send(Body).expect(200);
+      await request.put('/home/like/:123').send(Body).expect(200);
       done();   
   })
 })
 
-describe('testing PUT with wrong userid',function(){
-  it('responds to POST /home/create_goal/:userid',async(done)=>{
+describe('testing PUT on like with wrong userid',function(){
+  it('responds to PUT /home/create_goal/:321',async(done)=>{
     const Body= {id: "321"}
-      await request.post('/home/like/:321').send(Body).expect(404);
+      await request.put('/home/like/:321').send(Body).expect(404);
       done();   
   })
 })
